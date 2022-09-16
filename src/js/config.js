9a85b5b26a9650de8ed1509ae1be326bfd8b5519
Michael Bardou <michael.bardou@icloud.com>
2022-09-16 16:17:31 +0200

    finish form

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const CONF = {
+  PUBLIC_KEY: "zkw_7mOYkXTY79xtJ",
+  SERVICE_ID: "service_c42mj2a",
+  TEMPLATE_ID: "template_yskn4op",
+};
--- src/js/email.js
+++ src/js/email.js
@@ -1,10 +1,24 @@
 "use strict";
 
-emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", "#myForm").then(
-  function (response) {
-    console.log("SUCCESS!", response.status, response.text);
-  },
-  function (error) {
-    console.log("FAILED...", error);
-  }
-);
+import { CONF } from "./config.js";
+
+emailjs.init(CONF.PUBLIC_KEY);
+
+function sendEmail(params) {
+  let form = document.getElementById("myform");
+
+  emailjs.sendForm(CONF.SERVICE_ID, CONF.TEMPLATE_ID, form).then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
+      document.getElementById("firstName").value = "";
+      document.getElementById("secondName").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+}
+
+document.querySelector(".submit-button").addEventListener("click", sendEmail);
