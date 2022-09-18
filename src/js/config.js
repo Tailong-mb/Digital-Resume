f4730f4e297c02e91b7b41acbb0ff1e121f138f5
Michael Bardou <michael.bardou@icloud.com>
2022-09-18 14:38:27 +0200

    copyright

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const CONF = {
-  PUBLIC_KEY: "zkw_7mOYkXTY79xtJ",
-  SERVICE_ID: "service_c42mj2a",
-  TEMPLATE_ID: "template_yskn4op",
+  PUBLIC_KEY: "WT3-5OtZi75kD6DqK",
+  SERVICE_ID: "service_wumfdrp",
+  TEMPLATE_ID: "template_xi5jugg",
 };
