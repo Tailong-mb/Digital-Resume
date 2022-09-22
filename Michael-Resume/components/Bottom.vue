25fc460f41af53d207e38a48496056889b7f6f38
Michael Bardou <michael.bardou@icloud.com>
2022-09-22 21:23:40 +0200

    start to refactor project

---
+++ Michael-Resume/components/Bottom.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="grid-container">
+    <div class="linkedin subtitle footer">
+      <a
+        href="https://www.linkedin.com/in/michael-bardou-732483238/"
+        class="element-footer"
+      >
+        <i class="fa-brands fa-linkedin"></i><span>LINKEDIN</span></a
+      >
+    </div>
+    <div class="github subtitle footer">
+      <a href="https://github.com/Tailong-mb" class="element-footer">
+        <i class="fa-brands fa-github"></i><span>GITHUB</span></a
+      >
+    </div>
+    <div class="date-updated footer subtitle element-footer">
+      <i class="fa-regular fa-copyright"></i><span>2022</span>
+    </div>
+  </footer>
+</template>
---
+++ Michael-Resume/components/Top.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="grid-container">
+    <div class="hero-header-logo title"><span>M</span></div>
+    <div class="hero-header-information">
+      <div class="title name is-bold">Michael BARDOU</div>
+      <div class="subtitle">22/11/1997</div>
+      <div class="subtitle">michael.bardou@icloud.com</div>
+    </div>
+    <div class="hero-header-presentation">
+      <p class="text">
+        I am a web developer who is passionate about building web applications.
+        I am experienced in both front-end and back-end web development, and I
+        love challenge myself with both technologies.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.hero-header-information {
+  grid-column: 4/7;
+  display: flex;
+  flex-direction: column;
+  gap: 1px;
+  align-self: center;
+}
+
+.name {
+  margin-bottom: 3px;
+}
+
+.hero-header-presentation {
+  grid-column: 8/13;
+  display: flex;
+  flex-direction: column;
+  gap: 1px;
+  align-self: center;
+}
+</style>
