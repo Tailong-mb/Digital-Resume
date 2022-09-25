ece71555446b6e04771bf5719a97cdb1d3590a06
Michael Bardou <michael.bardou@icloud.com>
2022-09-25 21:33:09 +0200

    update global variable

--- components/Experience.vue
+++ components/Experience.vue
@@ -13,7 +13,7 @@ const moveToForm = () => {
 
 const sendEmail = () => {
   const form = document.querySelector("form");
-  emailjs.sendForm(EMAILSERVICE, EMAILTEMPLATE, form, EMAILKEY).then(
+  emailjs.sendForm(process.env.EMAILSERVICE, process.env.EMAILTEMPLATE, form, process.env.EMAILKEY).then(
     function (response) {
       console.log("SUCCESS!", response.status, response.text);
     },
